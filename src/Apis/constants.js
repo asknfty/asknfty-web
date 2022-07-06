1347a395c4211b501ba17991ff777dbf940ef353
namnlcDev <100658696+namnlcDev@users.noreply.github.com>
2022-07-06 09:28:30 +0700

    test ccors error

--- src/Apis/constants.js
+++ src/Apis/constants.js
@@ -2,7 +2,8 @@ const END_POINT = {
     //nft
     NFT: {
         // search nft collection with queries, filters, sorts
-        GET_ALL_NFT_COLLECTION: 'nft-collections',
+        // GET_ALL_NFT_COLLECTION: 'nft-collections',
+        GET_ALL_NFT_COLLECTION: '',
         // get details collection with id
         GET_DETAILS_NFT_COLLECTION: (nftCollectionsId) => `nft-collections/${nftCollectionsId}`,
         // get details nft item
