bc32dafd29586c11c20a636272bf329f21544ebb
Tai Vo <taivt@bap.jp>
2022-07-25 11:21:59 +0700

    Show image preview with image have type video and mp3

---
+++ src/Constants/image.js
@@ -0,0 +1,5 @@
+export const IMAGE_TYPES = {
+  VIDEO: 'video',
+  IMAGE: 'image',
+  MP3: 'mp3'
+}
--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,3 +1,4 @@
 export * from './home'
 export * from './api'
 export * from './pag'
+export * from './image'
